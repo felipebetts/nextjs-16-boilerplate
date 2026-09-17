6d0a7148496d945e3c46c1f44c52a1f0289f90e0
Felipe <felipe.a.betts@gmail.com>
2026-09-16 23:24:02 -0300

    feat: aplica o `pnpm format`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-import jsxA11y from "eslint-plugin-jsx-a11y";
-import checkFile from "eslint-plugin-check-file";
-import prettier from "eslint-config-prettier/flat";
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier/flat'
+import checkFile from 'eslint-plugin-check-file'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -16,25 +16,25 @@ const eslintConfig = defineConfig([
   },
   {
     plugins: {
-      "check-file": checkFile,
+      'check-file': checkFile,
     },
     rules: {
-      "prefer-arrow-callback": ["error"],
-      "prefer-template": ["error"],
-      "check-file/filename-naming-convention": [
-        "error",
+      'prefer-arrow-callback': ['error'],
+      'prefer-template': ['error'],
+      'check-file/filename-naming-convention': [
+        'error',
         {
-          "**/*.{ts,tsx}": "KEBAB_CASE",
+          '**/*.{ts,tsx}': 'KEBAB_CASE',
         },
         {
           // ignore the middle extensions of the filename to support filename like bable.config.js or smoke.spec.ts
           ignoreMiddleExtensions: true,
         },
       ],
-      "check-file/folder-naming-convention": [
-        "error",
+      'check-file/folder-naming-convention': [
+        'error',
         {
-          "src/app/**": "NEXT_JS_APP_ROUTER_CASE",
+          'src/app/**': 'NEXT_JS_APP_ROUTER_CASE',
         },
       ],
     },
@@ -44,11 +44,11 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
-]);
+])
 
-export default eslintConfig;
+export default eslintConfig
